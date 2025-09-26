a42a18fee6b3b0376aa8c8b77fba8b94c1504f90
Victor Varza <victor.varza@gmail.com>
2025-09-26 21:21:34 +0300

    Update Docusaurus config for new URL and base path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://hlesey.github.io',
+  url: 'https://qedzone.ro',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
